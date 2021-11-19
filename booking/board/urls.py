558f12e206b1c483adc97cf33f5bf83bfbf4f584
maxim <anapa.igor@yandex.ru>
2021-11-19 18:36:10 +0300

    -reformat;

--- booking/board/urls.py
+++ booking/board/urls.py
@@ -10,15 +10,18 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
+
     path('user/all', views.UserListView.as_view()),
     path('user/<int:pk>', views.UserRetrieveUpdateView.as_view()),
     path('user/update/<int:pk>', views.UserUpdateView.as_view()),
     path('user/destroy/<int:pk>', views.UserDestroyView.as_view()),
     path('user/new', views.UserCreateView.as_view()),
+
     path('service/<int:pk>', views.ServiceRetrieveUpdateView.as_view()),
     path('service/update/<int:pk>', views.ServiceUpdateView.as_view()),
     path('service/all', views.ServiceListView.as_view()),
     path('service/new', views.ServiceCreateView.as_view()),
+
     path('rate/<int:pk>', views.RateRetrieveUpdateView.as_view()),
     path('rate/update/<int:pk>', views.RateUpdateView.as_view()),
     path('rate/all', views.RateListViewSet.as_view()),
